60802ef496aea0fcdd0b671f802cf1c7f81a8ce2
David Hull <david.hull37@gmail.com>
2019-03-11 11:37:52 -0500

    Added the polls application with the first route

---
+++ polls/urls.py
@@ -0,0 +1,7 @@
+# This is a URLconf file
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index')
+]
---
+++ polls/views.py
@@ -0,0 +1,6 @@
+# TODO: looke up django shorcuts
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, World. You're at the polls index!")
