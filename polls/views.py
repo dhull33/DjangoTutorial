a4d94496805d3fc5230e4535073daa26f55c064d
David Hull <david.hull37@gmail.com>
2019-03-13 11:31:02 -0500

    Added the results.html template

--- polls/views.py
+++ polls/views.py
@@ -25,12 +25,10 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', { 'question': question })
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', { 'question': question })
 
 def vote(request, question_id):
-    print('YOU JUST VOTED!!!!!!')
-    print(request)
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get( pk=request.POST['choice'] )
